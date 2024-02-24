1fc6b77f94591ddec3a2d00429cf3d7a25c521c8
aquariusjs <1015772292@qq.com>
2024-02-24 14:19:30 +0800

    feat:log

---
+++ src/utils/date.js
@@ -0,0 +1,30 @@
+export const format = date => {
+	date = new Date(date * 1000); // 将秒数转换为毫秒数
+
+	let year = date.getFullYear();
+	let month = date.getMonth() + 1;
+	let day = date.getDate();
+	let hours = date.getHours();
+	let minutes = date.getMinutes();
+	let seconds = date.getSeconds();
+
+	return (
+		year +
+		"-" +
+		addZero(month) +
+		"-" +
+		addZero(day) +
+		" " +
+		addZero(hours) +
+		":" +
+		addZero(minutes) +
+		":" +
+		addZero(seconds)
+	);
+};
+function addZero(num) {
+	return num < 10 ? "0" + num : num;
+}
+let timestamp = 1619569282000; // 举例一个时间戳
+let datetime = format(timestamp);
+console.log(datetime);
--- src/views/FrontLog/index.vue
+++ src/views/FrontLog/index.vue
@@ -1,24 +1,178 @@
 <script setup>
 	import { ref, onMounted } from "vue";
 	import { FrontLogAPI } from "@/apis/frontLog";
+	import { isEqual, round } from "lodash";
+	import { format } from "@/utils/date";
 	const page = ref(10);
 	const pageNum = ref(1);
+	const total = ref(0);
 	const logList = ref();
 	const getLogList = async () => {
 		const res = await FrontLogAPI.getAll({ page: page.value, pageNum: pageNum.value });
-        console.log(res)
+		console.log(res);
+		logList.value = res.data;
+		if (pageNum.value === 1) {
+			total.value = res.count;
+		}
+	};
+	const currentChangeHandler = _pageNum => {
+		pageNum.value = _pageNum;
+		getLogList();
+	};
+	const sizeChangeHandler = _page => {
+		page.value = _page;
+		getLogList();
 	};
 	onMounted(() => {
-        getLogList()
-    });
+		getLogList();
+	});
 </script>
 
 <template>
 	<div class="front-log">
-		<el-table :data="logList" style="width: 100%">
-			<el-table-column prop="prop" label="label" width="width"> </el-table-column>
+		<el-table :data="logList" style="width: 100%" height="570">
+			<el-table-column type="expand">
+				<template #default="{ row }">
+					<div class="expand">
+						<p>
+							<span>{{ row.res.priceSystem }}</span>
+							<el-divider direction="vertical" />
+							<span>{{ row.res.department }}</span>
+							<el-divider direction="vertical" />
+							<span v-if="row.res.onepieceforshipping">
+								<span>{{ row.res.onepieceforshipping }}</span>
+								<el-divider direction="vertical" />
+							</span>
+							<span>{{ row.res.platform }}</span>
+							<el-divider direction="vertical" />
+							<span>{{ row.res.transportationMethod }}</span>
+						</p>
+						<p>
+							<span>
+								<span>售价：</span>
+								<span>{{ row.res.money }} ￥</span>
+							</span>
+							<el-divider direction="vertical" />
+							<span>
+								<span>费比佣金：</span>
+								<span>{{ row.res.feeRatio }} ￥</span>
+							</span>
+							<el-divider direction="vertical" />
+							<span>
+								<span>运费：</span>
+								<span>{{ row.res.freight }} ￥</span>
+							</span>
+						</p>
+						<p>
+							<span>
+								<span>财务成本：</span>
+								<span>{{ row.res.financialProductionCosts }} ￥</span>
+							</span>
+							<el-divider direction="vertical" />
+							<span>
+								<span>财务利润目标：</span>
+								<span>{{ row.res.financialProfitTarget * 100 }}</span>
+								<span> %</span>
+							</span>
+							<el-divider direction="vertical" />
+							<span>
+								<span>财务利润率：</span>
+								<span>{{ round(row.res.financialProfitMargins * 100, 2) }}</span>
+								<span> %</span>
+							</span>
+						</p>
+						<p>
+							<span>
+								<span>业务成本：</span>
+								<span>{{ row.res.businessProductionCosts }} ￥</span>
+							</span>
+							<el-divider direction="vertical" />
+							<span>
+								<span>业务利润目标：</span>
+								<span>{{ row.res.businessProfitTarget * 100 }}</span>
+								<span> %</span>
+							</span>
+							<el-divider direction="vertical" />
+							<span>
+								<span>业务利润率：</span>
+								<span>{{ round(row.res.businessProfitMargins * 100, 2) }}</span>
+								<span> %</span>
+							</span>
+						</p>
+						<div v-if="row.res.product && !isEqual(row.res.product?.length, 0)">
+							<h4>商品列表:</h4>
+							<el-table :data="row.res.product" border>
+								<el-table-column label="名称" prop="name" />
+								<el-table-column label="数量" prop="num" />
+							</el-table>
+						</div>
+						<div v-if="row.res.giftProduct && !isEqual(row.res.giftProduct?.length, 0)">
+							<h4>赠品列表:</h4>
+							<el-table :data="row.res.product" border>
+								<el-table-column label="名称" prop="name" />
+								<el-table-column label="数量" prop="num" />
+							</el-table>
+						</div>
+						<div
+							v-if="
+								row.res.specialProduct &&
+								!isEqual(row.res.specialProduct?.length, 0)
+							"
+						>
+							<h4>特殊配赠列表:</h4>
+							<el-table :data="row.res.product" border>
+								<el-table-column label="名称" prop="name" />
+								<el-table-column label="数量" prop="num" />
+								<el-table-column label="价格" prop="cost" />
+							</el-table>
+						</div>
+					</div>
+				</template>
+			</el-table-column>
+			<el-table-column label="查询时间">
+				<template #default="{ row }">
+					<span>{{ format(row.create_time) }}</span>
+				</template>
+			</el-table-column>
+			<el-table-column label="财务利润率">
+				<template #default="{ row }">
+					<span>{{ round(row.res.financialProfitMargins * 100, 2) }}</span>
+					<span> %</span>
+					<span>{{ " (" + row.res.financialProfitTarget * 100 + "%)" }}</span>
+				</template>
+			</el-table-column>
+			<el-table-column label="业务利润率">
+				<template #default="{ row }">
+					<span>{{ round(row.res.businessProfitMargins * 100, 2) }}</span>
+					<span> %</span>
+					<span>{{ " (" + row.res.businessProfitTarget * 100 + "%)" }}</span>
+				</template>
+			</el-table-column>
 		</el-table>
+		<div class="pagination_container">
+			<el-pagination
+				v-model:current-page="pageNum"
+				v-model:page-size="page"
+				layout="prev, pager, next, sizes, total"
+				:page-sizes="[10, 20, 50, 100]"
+				:total="total"
+				@size-change="sizeChangeHandler"
+				@current-change="currentChangeHandler"
+			/>
+		</div>
 	</div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+	.front-log {
+		.expand {
+			display: flex;
+			flex-direction: column;
+			gap: 5px;
+			padding: 10px 30px;
+		}
+		.pagination_container {
+			padding-top: 10px;
+		}
+	}
+</style>
